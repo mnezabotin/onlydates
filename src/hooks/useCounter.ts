91696bef8c95f76da430d42b79d0d3d66a805771
maxim.mochalov <maxim.mochalov@akvelon.com>
2025-08-10 19:04:17 +0300

    fixed counter hook

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,25 +1,27 @@
 import { useEffect, useState } from 'react'
 
 export const useCounter = (endValue = 2000, duration = 1000) => {
-  const [count, setCount] = useState(2000);
+  const [count, setCount] = useState(0)
+  const [prev, setPrev] = useState(2000)
 
   useEffect(() => {
     let startTime: number
     let animationFrameId: number
 
     const animate = (currentTime: number) => {
-      if (!startTime) startTime = currentTime;
+      if (!startTime) startTime = currentTime
       const progress = (currentTime - startTime) / (duration)
 
       if (progress < 1) {
-        setCount(Math.floor(progress * endValue))
+        setCount(Math.floor(progress * (endValue - prev) + prev))
         animationFrameId = requestAnimationFrame(animate)
       } else {
         setCount(endValue)
+        setPrev(endValue)
       }
-    };
+    }
 
-    animationFrameId = requestAnimationFrame(animate);
+    animationFrameId = requestAnimationFrame(animate)
 
     return () => cancelAnimationFrame(animationFrameId)
   }, [endValue, duration])
